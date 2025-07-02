ee616fd43a1072dfba26c744c59198c8f85cc90a
Danil <danilom31@mail.ru>
2025-07-02 20:18:41 +0300

    w

--- index.js
+++ index.js
@@ -2,7 +2,38 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('<h1>Привет, Октагон!</h1>');
+  response.send('<h1>Привет, октагон!</h1>');
 });
+
+app.get('/static', (request, response) => {
+    response.send({
+        header: "Hello",
+        body: "Octagon NodeJS Test"
+    });
+});
+
+app.get('/dynamic', (request, response) => {
+    const { a, b, c } = request.query;
+    
+    if (a == undefined || b == undefined || c == undefined ||     isNaN(parseFloat(a)) || !isFinite(a) ||
+    isNaN(parseFloat(b)) || !isFinite(b) ||
+    isNaN(parseFloat(c)) || !isFinite(c)) {
+        return response.send({
+            header: "Error"
+        });
+    }
+
+    const numA = parseFloat(a);
+    const numB = parseFloat(b);
+    const numC = parseFloat(c);
+    const result = (numA * numB * numC) / 3;
+
+    response.send({
+        header: "Calculated",
+        body: result.toString()
+    });
+});
+
+
 
 app.listen(3000);         
