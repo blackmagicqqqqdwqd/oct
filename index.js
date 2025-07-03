4fce093a74f5369a1767ca8be461b11b4c053cf3
Danil <danilom31@mail.ru>
2025-07-03 19:45:25 +0300

    w

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const mysql = require("mysql2");
+  
+
+
+
 app.get('/', (request, response) => {
   response.send('<h1>Привет, октагон!</h1>');
 });
@@ -11,6 +16,7 @@ app.get('/static', (request, response) => {
         body: "Octagon NodeJS Test"
     });
 });
+
 
 app.get('/dynamic', (request, response) => {
     const { a, b, c } = request.query;
@@ -31,6 +37,127 @@ app.get('/dynamic', (request, response) => {
     response.send({
         header: "Calculated",
         body: result.toString()
+    });
+});
+
+
+
+ 
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "ChatBotTests"
+});
+
+connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
+
+
+
+
+app.get('/getAllItems', (request, response) => {
+    
+        connection.query("SELECT * FROM items", function(error, results)  {
+            
+            if (error) {
+            response.send(null);
+            } else {
+            response.send(results); 
+            }
+            
+        });
+});
+
+
+app.post('/addItem', (request, response) => {
+    const { name, desc } = request.query;
+    
+    
+    if (!name || !desc) {
+        return response.send(null);
+    }
+
+    const sql = 'INSERT INTO Items (name, `desc`) VALUES (?, ?)';
+    
+    connection.query(sql, [name, desc], function(err) {
+       
+        if (err) return response.send(null);
+        
+        response.send({
+                name: name,
+                desc: desc
+        });
+    });
+});
+
+
+app.post('/deleteItem', (request, response) => {
+    const { id } = request.query;
+    
+    // Проверка наличия обязательного параметра
+    if (!id || isNaN(parseInt(id))) {
+        return response.send(null);
+    }
+
+    const sql = 'DELETE FROM Items WHERE id = ?';
+    
+    connection.query(sql, [id], function(err, results) {
+        if (err) return response.send(null);
+        
+        if (results.affectedRows === 0) {
+            return response.send({}); 
+        }
+        
+        response.send({
+                id: id
+        });
+        
+    });
+});
+
+
+
+
+app.post('/updateItem', (request, response) => {
+    const { id, name, desc } = request.query;
+
+  
+    if (!id || isNaN(parseInt(id))) {
+        return response.send(null);
+    }
+    if (!name && !desc) {
+        return response.send(null);
+    }
+
+    
+    let sql = 'UPDATE Items SET ';
+    const params = [];
+    let updates = [];
+    
+    if (name) {
+        updates.push('name = ?');
+        params.push(name);
+    }
+    if (desc) {
+        updates.push('`desc` = ?');
+        params.push(desc);
+    }
+    
+    sql += updates.join(', ') + ' WHERE id = ?';
+    params.push(id);
+
+  
+    connection.query(sql, params, function(err, results) {
+        if (err) response.send(null);
+        if (results.affectedRows === 0) { return response.send({}); }
+        response.send({id: id, name:name , desc:desc});
     });
 });
 
